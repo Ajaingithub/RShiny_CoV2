1ace94cd0cd36de54de325e7fdc64b2ecfba3f48
Abhinav Jain <37553954+Ajaingithub@users.noreply.github.com>
2024-11-12 15:39:27 -0800

    Create cell_express_front_splitted_Samples.R

---
+++ Data/cell_express_front_splitted_Samples.R
@@ -0,0 +1,26 @@
+featureplot_front_split_sample <- function(obj, feature, reduction = "umap", x="UMAP_1", y="UMAP_2",size=0.2,color="blue",ncol=5,nrow=3){
+  p <- FeaturePlot(obj, feature, reduction = reduction)
+  stopifnot(all(rownames(p$data) == rownames(obj@meta.data)))
+  p$data$vaccine = obj@meta.data$vaccine
+  p$data$orig.ident = obj@meta.data$orig.ident
+  p$data$samples=paste(p$data$orig.ident,substr(p$data$vaccine,1,1),sep = "_")
+  
+  feature <- gsub("-",".",feature)
+  p2 <- ggplot(p$data[order(p$data[feature]),], aes_string(x=x, y=y, color=feature)) + 
+    facet_wrap(~ samples,ncol = ncol, nrow = nrow) +
+    geom_point(size=size) + 
+    scale_colour_gradientn(
+      colours = c("lightgrey",color),
+      limits=c(0,max(p$data[feature])),
+      oob = scales::squish) + 
+    ggtitle(paste(feature,"expressed front", sep = " ")) +
+    theme(plot.title = element_text(hjust = 0.5),
+          axis.text=element_text(size=10),
+          panel.background = element_rect(fill = 'white', colour = 'black'),
+          legend.text=element_text(size=10),
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.y = element_blank())
+  return(p2)
+}
